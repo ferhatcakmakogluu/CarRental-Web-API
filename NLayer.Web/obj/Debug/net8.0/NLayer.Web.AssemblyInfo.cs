fe8d9a3ef844be3df54344935f9fcdb8ffeaa8fe
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-07-21 23:11:47 +0300

    mvc

--- NLayer.Web/obj/Debug/net8.0/NLayer.Web.AssemblyInfo.cs
+++ NLayer.Web/obj/Debug/net8.0/NLayer.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c323c2b2c89ef5519c8b0188bd7c4b9925f9af9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b2611f85a4696a8026e76cded222ebb0fdcf23")]
 [assembly: System.Reflection.AssemblyProductAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
