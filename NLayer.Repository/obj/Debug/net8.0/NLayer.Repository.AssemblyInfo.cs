bf404090cc43a6c03b44d29413b7f5d5a6ddf284
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-07-21 23:22:06 +0300

    update

--- NLayer.Repository/obj/Debug/net8.0/NLayer.Repository.AssemblyInfo.cs
+++ NLayer.Repository/obj/Debug/net8.0/NLayer.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NLayer.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8d9a3ef844be3df54344935f9fcdb8ffeaa8fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e792e53ba95e299eb083196a825a3585ff7162db")]
 [assembly: System.Reflection.AssemblyProductAttribute("NLayer.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NLayer.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
